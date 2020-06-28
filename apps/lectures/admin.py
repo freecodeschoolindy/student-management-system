48fa8c103ae54f3b5a27034871d63f02f01a7710
gwenf <gwenfaraday@pm.me>
2020-06-28 13:57:21 -0400

    "code from livestream on June 28, part 2"
    This reverts commit e23733e8eedf17b86f169f184bbfab0d44080b8b.

--- apps/lectures/admin.py
+++ apps/lectures/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Lecture
+from .models import Lecture, Attendance
 
 
 class LectureAdmin(admin.ModelAdmin):
@@ -7,4 +7,24 @@ class LectureAdmin(admin.ModelAdmin):
     search_fields = ('title', 'lecturer_name')
 
 
+class AttendanceAdmin(admin.ModelAdmin):
+    list_display = ('lecture', 'get_student', 'get_date')
+    search_fields = ('student__userprofile__first_name',
+                     'lecture__title')
+
+    def get_student(self, obj):
+        return obj.student.userprofile.first_name
+    get_student.admin_order_field = 'student'
+    get_student.short_description = 'Student\'s Name'
+
+    def get_date(self, obj):
+        return obj.lecture.date
+    get_date.admin_order_field = 'lecture'
+    get_date.short_description = 'Date'
+    # list_display = ('lecture__title', 'student__userprofile__first_name',
+    #                 'lecture__date')
+    # search_fields = ('title', 'lecturer_name')
+
+
 admin.site.register(Lecture, LectureAdmin)
+admin.site.register(Attendance, AttendanceAdmin)
--- apps/lectures/models.py
+++ apps/lectures/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 from apps.utils.models import AbstractTableMeta
 
@@ -14,3 +15,11 @@ class Lecture(AbstractTableMeta, models.Model):
     level = models.IntegerField(choices=((1, 'Level 1'), (2, 'Level 2')),
                                 default=1)
     required = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Attendance(AbstractTableMeta, models.Model):
+    lecture = models.ForeignKey(Lecture, on_delete=models.CASCADE)
+    student = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
