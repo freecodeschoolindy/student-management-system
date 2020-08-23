6e34a61477317a94955bab4bef7594b1a1d97cd2
gwenf <gwenfaraday@pm.me>
2020-08-23 16:18:21 -0400

    code cleanup for nested routes

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -15,19 +15,11 @@ assignments_router.register(
     basename='submissions'
 )
 
-profile_router = routers.NestedSimpleRouter(router, r'', lookup='list')
-profile_router.register(
-    r'profile',
-    UserProfileView,
-    basename='profile'
-)
-
 urlpatterns = [
     path('', include(router.urls)),
     path('<int:pk>/profile/',
          UserProfileView.as_view(), name='user-profile'),
     path('', include(assignments_router.urls)),
-    # path('', include(profile_router.urls)),
     path('login', CustomAuthToken.as_view()),
     # path('authorize', CustomAuthToken.as_view()),
 ]
